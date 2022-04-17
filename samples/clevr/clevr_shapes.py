b771865646554c8a6775adcb3a954e5976f900cc
sventhijssen <thijssen_sven@hotmail.com>
2022-04-17 19:13:01 -0400

    CLEVR Mask RCNN for shapes and colors

---
+++ samples/clevr/clevr_shapes.py
@@ -0,0 +1,164 @@
+"""
+Mask R-CNN
+Configurations and data loading code for the synthetic Shapes dataset.
+This is a duplicate of the code in the noteobook train_shapes.ipynb for easy
+import into other notebooks, such as inspect_model.ipynb.
+
+Copyright (c) 2017 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+"""
+import json
+import os
+import re
+import sys
+import math
+import random
+from os import listdir
+from pathlib import Path
+
+import numpy as np
+import cv2
+import torch
+from cv2 import imread
+from torchvision.io import read_image
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import utils
+
+
+class CLEVRShapeConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "shapes"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 8
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 3  # background + 3 shapes
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 128
+    IMAGE_MAX_DIM = 128
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 32
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 100
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 5
+
+
+class CLEVRShapeDataset(utils.Dataset):
+    """Generates the shapes synthetic dataset. The dataset consists of simple
+    shapes (triangles, squares, circles) placed randomly on a blank surface.
+    The images are generated on the fly. No file access required.
+    """
+
+    # Partially based on:
+    # https://towardsdatascience.com/object-detection-using-mask-r-cnn-on-a-custom-dataset-4f79ab692f6d
+    def load_dataset(self, dataset_dir, is_train=True):
+
+        # Add classes
+        self.add_class("shapes", 1, "cube")
+        self.add_class("shapes", 2, "sphere")
+        self.add_class("shapes", 3, "cylinder")
+
+        # For each image in the dataset
+        for filename in listdir(dataset_dir):
+
+            # Skip mask files
+            if "mask" in filename:
+                continue
+
+            # Name of image file
+            name = filename[:-4]
+
+            # Image ID
+            image_id = name[10:]
+
+            # Image path
+            img_path = dataset_dir + filename
+
+            if is_train and int(image_id) >= 90:
+                continue
+
+            if not is_train and int(image_id) < 90:
+                continue
+
+            self.add_image("shapes", image_id=image_id, path=img_path)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for shapes of the given image ID.
+        """
+
+        info = self.image_info[image_id]
+
+        # ! Careful ! Not the same as the given image ID (probably due to training/val split?
+        file_image_id = int(info.get("id"))
+
+        img_path = info["path"]
+        dataset_dir = str(Path(img_path).parent)
+        scenes_dir = str(Path(img_path).parent.parent.joinpath("scenes"))
+
+        filename = Path(img_path).name
+
+        # Name of image file
+        name = filename[:-4]
+
+        scene_path = scenes_dir + "/" + name + ".json"
+
+        with open(scene_path, "r") as f:
+            json_data = json.load(f)
+
+        txt_image_filename = "".join(listdir(dataset_dir))
+
+        mask = np.zeros([320, 480, len(json_data["objects"])], dtype=np.uint8)
+
+        class_ids = np.zeros(len(json_data["objects"]))
+
+        for k in range(len(json_data["objects"])):
+            # mask_path = images_dir + name + "_mask.png"
+            # mask = read_image(mask_path)
+            # obj_ids = torch.unique(mask)
+            match = re.findall(r'CLEVR_new_%s_%d_mask_(\w+).png' % (str(file_image_id).zfill(6), k), txt_image_filename)
+            shape = match[0]
+            mask_filename = "CLEVR_new_%s_%d_mask_%s.png" % (str(file_image_id).zfill(6), k, shape)
+            mask_path = dataset_dir + "/" + mask_filename
+            raw_mask = imread(mask_path)
+
+            for i in range(320):
+                for j in range(480):
+                    if raw_mask[i][j][0] == 255:
+                        mask[i][j][k] = 1
+
+            # We set the class id for the shape i
+            class_ids[k] = self.class_names.index(shape)
+
+        return mask, class_ids.astype(np.int32)
+
+    def image_reference(self, image_id):
+        """Return the shapes data of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "shapes":
+            return info["shapes"]
+        else:
+            super(self.__class__).image_reference(self, image_id)
---
+++ samples/clevr/clevr_shapes_main.py
@@ -0,0 +1,173 @@
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Root directory of the project
+from samples.clevr.clevr_shapes import CLEVRShapeDataset, CLEVRShapeConfig
+
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+from mrcnn.model import log
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Directory to load images
+DATA_DIR = os.path.expanduser('~') + "/Documents/ucf/2022-spring/cap6121/clevr-dataset-gen/clevr-dataset-gen/datasets/shapes/images/"
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "clevr_shape_mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+
+def get_ax(rows=1, cols=1, size=8):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+
+    Change the default size attribute to control the size
+    of rendered images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size * cols, size * rows))
+    return ax
+
+
+config = CLEVRShapeConfig()
+config.display()
+
+# Training dataset
+dataset_train = CLEVRShapeDataset()
+dataset_train.load_dataset(DATA_DIR, is_train=True)
+dataset_train.prepare()
+print("Training: %d" % len(dataset_train.image_ids))
+
+# Validation dataset
+dataset_val = CLEVRShapeDataset()
+dataset_val.load_dataset(DATA_DIR, is_train=False)
+dataset_val.prepare()
+print("Validation: %d" % len(dataset_val.image_ids))
+
+# Load and display random samples
+image_ids = np.random.choice(dataset_train.image_ids, 2)
+
+for image_id in image_ids:
+    image = dataset_train.load_image(image_id)
+    mask, class_ids = dataset_train.load_mask(image_id)
+    visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
+
+# Create model in training mode
+model = modellib.MaskRCNN(mode="training", config=config,
+                          model_dir=MODEL_DIR)
+
+# Which weights to start with?
+init_with = "coco"  # imagenet, coco, or last
+
+if init_with == "imagenet":
+    model.load_weights(model.get_imagenet_weights(), by_name=True)
+elif init_with == "coco":
+    # Load weights trained on MS COCO, but skip layers that
+    # are different due to the different number of classes
+    # See README for instructions to download the COCO weights
+    model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
+                                "mrcnn_bbox", "mrcnn_mask"])
+elif init_with == "last":
+    # Load the last model you trained and continue training
+    model.load_weights(model.find_last(), by_name=True)
+
+# Train the head branches
+# Passing layers="heads" freezes all layers except the head
+# layers. You can also pass a regular expression to select
+# which layers to train by name pattern.
+model.train(dataset_train, dataset_val,
+            learning_rate=config.LEARNING_RATE,
+            epochs=1,
+            layers='heads')
+
+# Fine tune all layers
+# Passing layers="all" trains all layers. You can also
+# pass a regular expression to select which layers to
+# train by name pattern.
+model.train(dataset_train, dataset_val,
+            learning_rate=config.LEARNING_RATE / 10,
+            epochs=2,
+            layers="all")
+
+
+class InferenceConfig(CLEVRShapeConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+
+inference_config = InferenceConfig()
+
+# Recreate the model in inference mode
+model = modellib.MaskRCNN(mode="inference",
+                          config=inference_config,
+                          model_dir=MODEL_DIR)
+
+# Get path to saved weights
+# Either set a specific path or find last trained weights
+# model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+model_path = model.find_last()
+
+# Load trained weights
+print("Loading weights from ", model_path)
+model.load_weights(model_path, by_name=True)
+
+# Test on a random image
+image_id = random.choice(dataset_val.image_ids)
+original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+    modellib.load_image_gt(dataset_val, inference_config,
+                           image_id, use_mini_mask=False)
+
+log("original_image", original_image)
+log("image_meta", image_meta)
+log("gt_class_id", gt_class_id)
+log("gt_bbox", gt_bbox)
+log("gt_mask", gt_mask)
+
+visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id,
+                            dataset_train.class_names, figsize=(8, 8))
+
+results = model.detect([original_image], verbose=1)
+
+r = results[0]
+visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'],
+                            dataset_val.class_names, r['scores'], ax=get_ax())
+
+# Compute VOC-Style mAP @ IoU=0.5
+# Running on 10 images. Increase for better accuracy.
+image_ids = np.random.choice(dataset_val.image_ids, 10)
+APs = []
+for image_id in image_ids:
+    # Load image and ground truth data
+    image, image_meta, gt_class_id, gt_bbox, gt_mask = \
+        modellib.load_image_gt(dataset_val, inference_config,
+                               image_id, use_mini_mask=False)
+    molded_images = np.expand_dims(modellib.mold_image(image, inference_config), 0)
+    # Run object detection
+    results = model.detect([image], verbose=0)
+    r = results[0]
+    # Compute AP
+    AP, precisions, recalls, overlaps = \
+        utils.compute_ap(gt_bbox, gt_class_id, gt_mask,
+                         r["rois"], r["class_ids"], r["scores"], r['masks'])
+    APs.append(AP)
+
+print("mAP: ", np.mean(APs))
